02ead223d59b57d3f89467ef950cfba0c37f0a15
Adam Greene <github@mzpqnxow.com>
2018-02-21 23:45:21 -0500

    Switch to dig to cover tcp/udp

--- validate_resolvers.sh
+++ validate_resolvers.sh
@@ -12,7 +12,7 @@
 #
 LIST=resolver_candidates.lst
 SWEEP_OUTFILE=$(mktemp)
-UP_OUTFILE=$(mktemp)
+UP_OUTFILE=up
 OUTFILE=resolvers-confirmed-$(date +%Y-%m-%d).lst
 PPS_RATE=2048
 TEST_DOMAIN=google.com
@@ -21,13 +21,27 @@ HOST_OPTIONS="-t a -W 2"
 # Otherwise, fill this in with the path to sudo
 SUDO=
 
-$SUDO masscan -iL "$LIST" -p U:53,53 -oG "$SWEEP_OUTFILE" --rate "$PPS_RATE"
-grep ^Host "$SWEEP_OUTFILE" | cut -d ' ' -f 2 | sort -u > "$UP_OUTFILE"
+rm -f $TCP_UP_OUTFILE $UDP_UP_OUTFILE
 
-for OPEN_SERVER in $(cat "$UP_OUTFILE")
+$SUDO masscan -iL "$LIST" -p U:53,53 -oG "$SWEEP_OUTFILE" --rate "$PPS_RATE"
+grep ^Host "$SWEEP_OUTFILE" | grep "open/tcp" | cut -d ' ' -f 2 | sort -u > "${PROTOCOL}-${UP_OUTFILE}"
+
+for PROTOCOL in udp tcp
 do
-    host $HOST_OPTIONS "$TEST_DOMAIN" "$OPEN_SERVER" 2>&1 1>/dev/null && echo "$OPEN_SERVER" | tee -a $OUTFILE
+    [[ "$PROTOCOL" -eq "udp" ]] && FLAG=no || FLAG=""
+    grep ^Host "$SWEEP_OUTFILE" | grep "open/udp" | cut -d ' ' -f 2 | sort -u > "${PROTOCOL}-{UP_OUTFILE}"
+    for OPEN_SERVER in $(cat "${PROTOCOL}-${UP_OUTFILE}")
+    do
+        echo -n "Try $OPEN_SERVER ..."
+        # host $HOST_OPTIONS "$TEST_DOMAIN" "$OPEN_SERVER" 2>&1 1>/dev/null && echo "$OPEN_SERVER" >> ${PROTOCOL}-OUTFILE
+        echo dig -t a +${FLAG}tcp $TEST_DOMAIN @$OPEN_SERVER 2>&1 1>/dev/null && echo "$OPEN_SERVER" >> ${PROTOCOL}-${OUTFILE}
+        if [ "$?" -eq "0" ]; then
+            echo " open !!"
+        else
+            echo " rejected !!"
+        fi
+    done
 done
-echo "DONE: $(wc -l $OUTFILE | cut -d ' ' -f 1) servers responding to queries"
-rm -f $SWEEP_OUTFILE $UP_OUTFILE
+# echo "DONE: $(wc -l $OUTFILE | cut -d ' ' -f 1) servers responding to queries"
+# rm -f $SWEEP_OUTFILE $UP_OUTFILE
 
